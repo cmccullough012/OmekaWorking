9bb58b8a1ac1e71b63a3e02cc01270f54495734e
cmccullough012 <cxm1880@rit.edu>
2018-08-22 12:28:08 -0400

    Hide oral history videos on mobile
    (temporary solution)

--- astrolabe-master/simple-pages/page/oral-histories.php
+++ astrolabe-master/simple-pages/page/oral-histories.php
@@ -3,10 +3,14 @@
 <h1><?php echo metadata('simple_pages_page', 'title');?></h1>
 
 <?php $text = metadata('simple_pages_page', 'text', array('no_escape' => true)); echo $this->shortcodes($text);?>
-<link href='https://cdn.knightlab.com/libs/soundcite/latest/css/player.css' rel='stylesheet' type='text/css'><script type='text/javascript' src='https://cdn.knightlab.com/libs/soundcite/latest/js/soundcite.min.js'></script>
-<div id = "easyPaginate">
-    <?php echo generate_video_mosaic('oral_text'); ?>
+
+    <div id = "oral-mobile">
+        <h3>In order to view our oral history videos, please visit our desktop site. We apologize for the inconvenience.</h3>
+    </div>
+<div id = "oral-histories-primary">
+    <div id = "easyPaginate">
+        <?php echo generate_video_mosaic('oral_text'); ?>
+    </div>
+
 </div>
-
-
 <?php fire_plugin_hook('public_items_browse', array('items' => $items, 'view' => $this)); ?>
