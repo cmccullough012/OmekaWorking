39137555be327d5e6cbbbe9acff54910ff3eab69
cmccullough012 <cxm1880@rit.edu>
2018-08-23 12:29:08 -0400

    Fixed jQuery issue with easyPaginate
    But I fixed it by commenting out something I thought was essential for juxtapose. Juxtapose seems to be working fine for now.

--- astrolabe-master/common/footer.php
+++ astrolabe-master/common/footer.php
@@ -22,7 +22,7 @@
         </div>
 
         <?php fire_plugin_hook('public_theme_footer'); ?>
-        <?php echo js_tag('script'); ?>
+        <?php echo js_tag('script');?>
         
         <!-- Juxtapose resources -->
         <script src="https://cdn.knightlab.com/libs/juxtapose/latest/js/juxtapose.min.js"></script>
@@ -33,9 +33,11 @@
         <script type='text/javascript' src='https://cdn.knightlab.com/libs/soundcite/latest/js/soundcite.min.js'></script>
         
         <!--install JQuery for the next script on Juxtapose -->
+        <!-- for some reason this broke the easy paginate plugin, and without it, juxtapose sitll seems to be working so 
+                        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
                 integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
-                crossorigin="anonymous"></script>
+                crossorigin="anonymous"></script>-->
 
     </footer><!-- end footer -->
 </body>
